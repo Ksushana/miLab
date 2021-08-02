c06556ad504d5789a8d30a3e4fa4acd3ab4a8dec
gramkin <bakluzhino@gmail.com>
2021-08-02 04:14:58 +0300

    SITE-10 (kits links)

--- app/scripts/modules/inquire-buttons.js
+++ app/scripts/modules/inquire-buttons.js
@@ -1,33 +1,31 @@
 import {LICENSING_EMAIL} from '../constants';
 
-(function () {
-  function buildLinkQuery({subject}) {
-    const parts = {
-      subject() {
-        return subject;
-      },
-      body() {
-        return `Hi MiLaboratories,
+function buildLinkQuery({subject}) {
+  const parts = {
+    subject() {
+      return subject;
+    },
+    body() {
+      return `Hi MiLaboratories,
 
 I am interested in your ${subject}.
 
 Could you provide more details?
 
 Best,`
-      }
-    };
-    return Object.keys(parts).map((name) => `${name}=${encodeURIComponent(parts[name]())}`).join('&');
-  }
+    }
+  };
+  return Object.keys(parts).map((name) => `${name}=${encodeURIComponent(parts[name]())}`).join('&');
+}
 
-  function updateLinks() {
-    document.querySelectorAll('button.inquire-button').forEach((el) => {
-      const subject = el.getAttribute('data-subject') || 'unknown';
-      el.addEventListener('click', (e) => {
-        e.stopPropagation();
-        location.href = 'mailto:' + LICENSING_EMAIL + '?' + buildLinkQuery({subject});
-      })
+function updateLinks() {
+  document.querySelectorAll('button.inquire-button').forEach((el) => {
+    const subject = el.getAttribute('data-subject') || 'unknown';
+    el.addEventListener('click', (e) => {
+      e.stopPropagation();
+      location.href = 'mailto:' + LICENSING_EMAIL + '?' + buildLinkQuery({subject});
     })
-  }
+  })
+}
 
-  document.addEventListener('DOMContentLoaded', updateLinks);
-})();
+document.addEventListener('DOMContentLoaded', updateLinks);
--- app/scripts/modules/software.js
+++ app/scripts/modules/software.js
@@ -1,47 +1,45 @@
 import {LICENSING_EMAIL} from '../constants';
 
-(function () {
-  let licenseType = 'academic';
+let licenseType = 'academic';
 
-  const parts = {
-    subject() {
-      return 'MiXCR Pro Trial Request';
-    },
-    body() {
-      return `Hi MiLaboratories,
+const parts = {
+  subject() {
+    return 'MiXCR Pro Trial Request';
+  },
+  body() {
+    return `Hi MiLaboratories,
 
 I would like to inquire ${licenseType} license.
 
 Best,`
-    }
-  };
-
-  function buildLinkQuery() {
-    return Object.keys(parts).map((name) => `${name}=${encodeURIComponent(parts[name]())}`).join('&');
   }
+};
 
-  function updateLinks() {
-    document.querySelectorAll('.pricing-block__btn').forEach((el) => {
-      el.setAttribute('href', 'mailto:' + LICENSING_EMAIL + '?' + buildLinkQuery());
-    })
-  }
+function buildLinkQuery() {
+  return Object.keys(parts).map((name) => `${name}=${encodeURIComponent(parts[name]())}`).join('&');
+}
 
-  function processForm(form) {
-    updateLinks();
+function updateLinks() {
+  document.querySelectorAll('.pricing-block__btn').forEach((el) => {
+    el.setAttribute('href', 'mailto:' + LICENSING_EMAIL + '?' + buildLinkQuery());
+  })
+}
 
-    form.addEventListener('click', (e) => {
-      e.stopPropagation();
-      form.querySelectorAll('input[type=radio]:checked').forEach((c) => {
-        licenseType = c.getAttribute('id');
-        updateLinks();
-      });
+function processForm(form) {
+  updateLinks();
+
+  form.addEventListener('click', (e) => {
+    e.stopPropagation();
+    form.querySelectorAll('input[type=radio]:checked').forEach((c) => {
+      licenseType = c.getAttribute('id');
+      updateLinks();
     });
-  }
-
-  document.addEventListener('DOMContentLoaded', function (event) {
-    const form = document.getElementById('license-type-switch-form');
-    if (form) {
-      processForm(form);
-    }
   });
-})();
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  const form = document.getElementById('license-type-switch-form');
+  if (form) {
+    processForm(form);
+  }
+});
